f86a9fd536367c22bc520cde4b841f2e82353019
Lee_junhee <jhl0893@gmail.com>
2020-05-30 02:06:08 +0900

    AI캐릭터 생성
    - Movement, Attack, Animation 제작

---
+++ Source/MonsterKindom/Enemy.cpp
@@ -0,0 +1,192 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Enemy.h"
+#include "Components/SphereComponent.h"
+#include "Components/BoxComponent.h"
+#include "Components/SkeletalMeshComponent.h"
+#include "AIModule/Classes/AIController.h"
+#include "PlayerCharacter.h"
+#include "Animation/AnimInstance.h"
+
+// Sets default values
+AEnemy::AEnemy()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	DetectSphere = CreateDefaultSubobject<USphereComponent>(TEXT("Detect Sphere"));
+	DetectSphere->SetupAttachment(GetRootComponent());
+	DetectSphere->InitSphereRadius(600.0f);
+
+	CombatSphere = CreateDefaultSubobject<USphereComponent>(TEXT("Combat Sphere"));
+	CombatSphere->SetupAttachment(GetRootComponent());
+	CombatSphere->InitSphereRadius(75.0f);
+
+	AttackHitBox = CreateDefaultSubobject<UBoxComponent>(TEXT("AttackHitBox"));
+	AttackHitBox->SetupAttachment(GetMesh(), TEXT("HitBoxSocket"));
+
+}
+
+// Called when the game starts or when spawned
+void AEnemy::BeginPlay()
+{
+	Super::BeginPlay();
+
+	AI_Controller = Cast<AAIController>(GetController());
+
+	DetectSphere->OnComponentBeginOverlap.AddDynamic(this, &AEnemy::DetecSphereOnBeginOverlap);
+	DetectSphere->OnComponentEndOverlap.AddDynamic(this, &AEnemy::DetecSphereOnEndOverlap);
+
+	CombatSphere->OnComponentBeginOverlap.AddDynamic(this, &AEnemy::CombatSphereOnBeginOverlap);
+	CombatSphere->OnComponentEndOverlap.AddDynamic(this, &AEnemy::CombatSphereOnEndOverlap);
+
+	AttackHitBox->OnComponentBeginOverlap.AddDynamic(this, &AEnemy::AttackHitBoxOverlap);
+}
+
+// Called every frame
+void AEnemy::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void AEnemy::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
+void AEnemy::DetecSphereOnBeginOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	if (!IsAlive)
+		return;
+
+	APlayerCharacter* MyChar = Cast<APlayerCharacter>(OtherActor);
+
+	if (MyChar)
+	{
+		TargetChar = MyChar;
+		MoveToTarget();	
+	}
+}
+
+void AEnemy::DetecSphereOnEndOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, 
+	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+	if (!IsAlive)
+		return;
+
+	APlayerCharacter* MyChar = Cast<APlayerCharacter>(OtherActor);
+
+	if (MyChar)
+	{
+		TargetChar = nullptr;
+		
+		if (AI_Controller)
+			AI_Controller->StopMovement();
+	}
+}
+
+void AEnemy::CombatSphereOnBeginOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, 
+	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	if (!IsAlive)
+		return;
+	APlayerCharacter* MyChar = Cast<APlayerCharacter>(OtherActor);
+
+	if (MyChar)
+	{
+		bTargetInAttackRange = true;
+
+		float AttackDelay = FMath::RandRange(AttackDelayMin, AttackDelayMax);
+		GetWorldTimerManager().SetTimer(AttackTimer, this, &AEnemy::Attack, AttackDelay);
+	}
+}
+
+void AEnemy::CombatSphereOnEndOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, 
+	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+	if (!IsAlive)
+		return;
+	APlayerCharacter* MyChar = Cast<APlayerCharacter>(OtherActor);
+	
+	if (MyChar)
+	{
+		bTargetInAttackRange = false;
+	}
+
+}
+
+void AEnemy::AttackHitBoxOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, 
+	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	
+}
+
+void AEnemy::MoveToTarget()
+{
+	if (bAttacking)
+		return;
+
+	if (AI_Controller)
+	{
+		FNavPathSharedPtr NavPath;
+		FAIMoveRequest MoveRequest;
+
+		MoveRequest.SetGoalActor(TargetChar);
+		MoveRequest.SetAcceptanceRadius(MoveToTargetRadius);
+
+		AI_Controller->MoveTo(MoveRequest, &NavPath);
+	}
+}
+
+void AEnemy::Attack()
+{
+	if (!IsAlive)
+		return;
+
+	if (bAttacking)
+		return;
+
+	bAttacking = true;
+	CanDetecDamageCollision = true;
+
+	if (AI_Controller)
+		AI_Controller->StopMovement();
+
+	auto AnimInstance = GetMesh()->GetAnimInstance();
+
+	if (AnimInstance && CombatMontage)
+	{
+		AnimInstance->Montage_Play(CombatMontage, 1.15f);
+		AnimInstance->Montage_JumpToSection(TEXT("Attack"), CombatMontage);
+		UE_LOG(LogTemp, Error, TEXT("asd"));
+	}
+}
+
+void AEnemy::AttackEnded()
+{
+	bAttacking = false;
+	CanDetecDamageCollision = false;
+
+	if (TargetChar)
+	{
+		if (bTargetInAttackRange)
+		{
+			float AttackDelay = FMath::RandRange(AttackDelayMin, AttackDelayMax);
+			GetWorldTimerManager().SetTimer(AttackTimer, this, &AEnemy::Attack, AttackDelay);
+		}
+		else
+		{
+			MoveToTarget();
+		}
+	}
+	else
+	{
+		bTargetInAttackRange = false;
+	}
+}
+
---
+++ Source/MonsterKindom/Enemy.h
@@ -0,0 +1,95 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "Enemy.generated.h"
+
+class USphereComponent;
+class UBoxComponent;
+class APlayerCharacter;
+class UAnimMontage;
+class AAIController;
+
+UCLASS()
+class MONSTERKINDOM_API AEnemy : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	AEnemy();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	UPROPERTY(EditAnywhere)
+		USphereComponent* DetectSphere;
+
+	UPROPERTY(EditAnywhere)
+		USphereComponent* CombatSphere;
+
+	UPROPERTY(EditAnywhere)
+		UBoxComponent* AttackHitBox;
+
+	UPROPERTY(BlueprintReadOnly)
+		bool IsAlive = true;
+
+	UFUNCTION()
+		virtual void DetecSphereOnBeginOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor,
+			UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	UFUNCTION()
+		virtual void DetecSphereOnEndOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor,
+			UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+
+	UFUNCTION()
+		virtual void CombatSphereOnBeginOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor,
+			UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	UFUNCTION()
+		virtual void CombatSphereOnEndOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor,
+			UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+
+	UFUNCTION()
+		virtual void AttackHitBoxOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor,
+			UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	APlayerCharacter* TargetChar;
+
+	AAIController* AI_Controller;
+
+	void MoveToTarget();
+
+	bool bAttacking;
+
+	UPROPERTY(EditAnywhere)
+		float MoveToTargetRadius = 10.0f;
+
+	UPROPERTY(EditAnywhere)
+		bool bTargetInAttackRange;
+	
+	float AttackDelayMin = 0.2;
+	float AttackDelayMax = 0.7f;
+
+	bool CanDetecDamageCollision;
+
+	void Attack();
+
+	UFUNCTION(BlueprintCallable)
+	void AttackEnded();
+
+	FTimerHandle AttackTimer;
+
+	UPROPERTY(EditAnywhere)
+		UAnimMontage* CombatMontage;
+};
